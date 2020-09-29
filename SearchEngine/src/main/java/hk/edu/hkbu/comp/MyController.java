4ff046c02347b23dde5563b075121d46d176081c
0.0 <57483422+a71496s03@users.noreply.github.com>
2020-09-30 00:39:10 +0800

    30/9/2020

--- SearchEngine/src/main/java/hk/edu/hkbu/comp/MyController.java
+++ SearchEngine/src/main/java/hk/edu/hkbu/comp/MyController.java
@@ -3,12 +3,11 @@ package hk.edu.hkbu.comp;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.HandlerMapping;
+
 
 @Controller
 public class MyController {
 
-	@Foos({@GetMapping("**"), @GetMapping("search")})
 	@GetMapping("**")
 	//@GetMapping("search")
 	@ResponseBody
@@ -16,8 +15,4 @@ public class MyController {
 		return String.format("You are browsing %s with %s!",
 			request.getRequestURI(), request.getQueryString());
 	}
-}
-
-@interface Foos {
-	GetMapping[] value();
 }
